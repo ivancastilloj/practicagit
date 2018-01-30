141dc4fcc2b681f60fcb68b815ac0c263c4f50f4
Curso mañana <Curso maana@PC24a5>
2018-01-30 14:32:41 +0100

    Trabajo Finalizado

---
+++ springapp/src/main/java/repository/ProductDao.java
@@ -0,0 +1,15 @@
+package repository;
+
+import java.util.List;
+
+import domain.Product;
+
+
+
+public interface ProductDao {
+
+    public List<Product> getProductList();
+
+    public void saveProduct(Product prod);
+
+}
--- springapp/src/main/java/services/SimpleProductManager.java
+++ springapp/src/main/java/services/SimpleProductManager.java
@@ -2,32 +2,39 @@ package services;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import domain.Product;
+import repository.ProductDao;
 
 
 
+@Component
 public class SimpleProductManager implements ProductManager {
 
     private static final long serialVersionUID = 1L;
 
-    private List<Product> products;
+    @Autowired
+    private ProductDao productDao;
+
+    public void setProductDao(ProductDao productDao) {
+        this.productDao = productDao;
+    }
 
     public List<Product> getProducts() {
-        return products; 
+        return productDao.getProductList();
     }
 
     public void increasePrice(int percentage) {
+        List<Product> products = productDao.getProductList();
         if (products != null) {
             for (Product product : products) {
                 double newPrice = product.getPrice().doubleValue() * 
                                     (100 + percentage)/100;
                 product.setPrice(newPrice);
+                productDao.saveProduct(product);
             }
-        }  
-    }   
-    
-	
-    public void setProducts(List<Product> products) {
-        this.products = products;
+        }
     }
 }
