9933fcc7e64a537a2a7db52a77bd2c2e126f5287
Curso mañana <Curso maana@PC24a5>
2018-01-30 12:14:34 +0100

    Inicio del MVC, Model, Services.

---
+++ springapp/src/main/java/services/ProductManager.java
@@ -0,0 +1,17 @@
+package services;
+
+
+import java.io.Serializable;
+import java.util.List;
+
+import domain.Product;
+
+
+
+public interface ProductManager extends Serializable {
+
+    public void increasePrice(int percentage);
+    
+    public List<Product> getProducts();
+
+}
--- springapp/src/main/java/springapp/InventoryController.java
+++ springapp/src/main/java/springapp/InventoryController.java
@@ -1,31 +1,54 @@
 package springapp;
 
+
+
 import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.util.Date;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import services.ProductManager;
+
+
+
 @Controller
-public class HelloController {
+public class InventoryController {
 
     protected final Log logger = LogFactory.getLog(getClass());
 
-    @RequestMapping(value="hello.htm")
+    @Autowired
+    private ProductManager productManager;
+
+    @RequestMapping(value="/hello.htm")
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        String now = (new Date()).toString();
+    	String now = (new Date()).toString();
         logger.info("Returning hello view with " + now);
 
-        return new ModelAndView("hello", "now", now);
+        Map<String, Object> myModel = new HashMap<String, Object>();
+        myModel.put("now", now);
+        myModel.put("products", this.productManager.getProducts());
 
+        return new ModelAndView("hello", "model", myModel);
     }
+
+
+    public void setProductManager(ProductManager productManager) {
+        this.productManager = productManager;
+    }
+
     
     @RequestMapping(value="servicios.htm")
     public ModelAndView llamarServicios(HttpServletRequest request, HttpServletResponse response)
@@ -34,7 +57,7 @@ public class HelloController {
         logger.info("Returning servicios view");
         String nombre="Juan";
 
-        return new ModelAndView("servicios.jsp", "minombre", nombre);
+        return new ModelAndView("servicios", "minombre", nombre);
     }
     
 
